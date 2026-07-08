a88b102646f14cd6a752863bef44a9af22867203
RafaelRaposoDeQuadros <rafaelraposodequadros@gmail.com>
2026-07-08 17:20:30 -0300

    Exception handling - delete

--- src/main/java/com/rafael/course/services/UserService.java
+++ src/main/java/com/rafael/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.rafael.course.services;
 
 import com.rafael.course.entities.User;
 import com.rafael.course.repositores.UserRepository;
+import com.rafael.course.services.exceptions.DatabaseException;
 import com.rafael.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,8 +31,12 @@ public class UserService {
         return repository.save(obj);
     }
 
-    public void delete(Long id){
-        repository.deleteById(id);
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
